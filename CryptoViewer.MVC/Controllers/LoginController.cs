e48abff8c98c42544b34ae00f9d5e7f7dccbb402
maxjke <m.jako030@gmail.com>
2024-06-11 22:48:42 +0300

    register and login page bones

---
+++ CryptoViewer.MVC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoViewer.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Route("/register")]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
