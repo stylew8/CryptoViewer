adca8838aa0680564da6cfe29f895e774f73d2ac
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-06-13 13:54:42 +0000

    yra

--- CryptoViewer.DAL/obj/Debug/net6.0/CryptoViewer.DAL.AssemblyInfo.cs
+++ CryptoViewer.DAL/obj/Debug/net6.0/CryptoViewer.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoViewer.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48abff8c98c42544b34ae00f9d5e7f7dccbb402")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoViewer.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoViewer.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
